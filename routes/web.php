e4cc4b2d507702d5e0483caec13467934d46f09d
SaDKinG <hker0004@gmail.com>
2023-08-26 23:11:58 +0100

    Added controller for home page, route for products

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\homeController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[homeController::class,'index'])->name('home');
+
+Route::resource('product', ProductController::class)->only('index', 'show');
