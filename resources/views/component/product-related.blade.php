125b80f5f75ac4d95befb45fa62e84a3ba0adfdd
SaDKinG <hker0004@gmail.com>
2023-08-26 23:09:31 +0100

    Created product related view

---
+++ resources/views/component/product-related.blade.php
@@ -0,0 +1,25 @@
+<div class="d-grid overflow-hidden">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <h2>Related Products</h2>
+            <hr>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-8">
+            <div class="row justify-content-center">
+                <div class="col-xl-3 col-md-4 bg-black bg-opacity-50 text-center rounded-4 p-1 m-2">
+                    <div class="card orangeBg p-3">
+                        <img src="{{ asset('assets/img/product4.png') }}" class="m-auto object-fit-contain img-fluid"
+                            alt="Image" style="width: 150px; height: 150px;">
+                        <div class="card-body">
+                            <h5 class="card-title">FOSIL ME3</h5>
+                            <p>Lorem ipsum dolor, sit amet consectetur</p>
+                            <h3 class="">$1534.00</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
