ae0fe398829e6c31ce40ac7684cb27391f9cb0c0
SaDKinG <hker0004@gmail.com>
2023-08-27 20:09:18 +0100

    added bootstrap-select

--- resources/views/layout/includes/head.blade.php
+++ resources/views/layout/includes/head.blade.php
@@ -4,6 +4,7 @@
     <link rel="shortcut icon" href="{{ versionedAsset('assets/img/favicon.png') }}" type="image/x-icon">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.14.0-beta3/css/bootstrap-select.min.css" integrity="sha512-g2SduJKxa4Lbn3GW+Q7rNz+pKP9AWMR++Ta8fgwsZRCUsawjPvF/BxSMkGS61VsR9yinGoEgrHPGPn2mrj8+4w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css" />
     <link rel="stylesheet" href="{{ versionedAsset('assets/css/styles.css') }}">
