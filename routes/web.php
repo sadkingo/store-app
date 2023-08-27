dd0699580f21d1133cf0bff443f9bcf8677dcdb0
SaDKinG <hker0004@gmail.com>
2023-08-27 20:10:31 +0100

    Added routes to login,register,logout

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\homeController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,6 +17,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/',[homeController::class,'index'])->name('home');
-
+Route::get('/', [homeController::class, 'index'])->name('home');
 Route::resource('product', ProductController::class)->only('index', 'show');
+
+
+Route::middleware('guest')->group(function ()
+    {
+    Route::get('login', [AuthController::class, 'index'])->name('login');
+    Route::post('login', [AuthController::class, 'store']);
+    Route::get('register', [RegisterController::class, 'index'])->name('register');
+    Route::post('register', [RegisterController::class, 'store']);
+    });
+
+Route::middleware('auth')->group(function ()
+    {
+    Route::get('logout', [AuthController::class, 'destroy'])->name('logout');
+    });
