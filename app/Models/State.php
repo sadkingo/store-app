778b4446eddf70b72744b7f5f1e9d50fedd405f2
SaDKinG <hker0004@gmail.com>
2023-08-27 20:07:07 +0100

    Added relation and fillable

--- app/Models/State.php
+++ app/Models/State.php
@@ -6,6 +6,16 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class State extends Model
-{
+    {
     use HasFactory;
-}
+    protected $fillable = [
+        'name'
+    ];
+
+    public function city()
+        {
+        return
+            $this->hasMany(City::class);
+        }
+
+    }
