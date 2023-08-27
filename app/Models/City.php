eca93f828843e73f8622c8e58320ef0adadb52f1
SaDKinG <hker0004@gmail.com>
2023-08-27 20:06:53 +0100

    Added fillable to city model

--- app/Models/City.php
+++ app/Models/City.php
@@ -8,6 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class City extends Model
     {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'state_id'
+    ];
     public function state()
         {
         return
