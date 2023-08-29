f9a2b72a87e0b7831b5ee26b08f87ba47882356f
SaDKinG <hker0004@gmail.com>
2023-08-29 22:47:49 +0100

    updated Helper

--- app/helpers/basicHelpers.php
+++ app/helpers/basicHelpers.php
@@ -1,10 +1,28 @@
 <?php
-function versionedAsset($path)
-{
-    if (app()->environment('local')) {
-        $version = rand();
-        return asset($path) . '?v=' . $version;
-    }
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
-    return asset($path);
-}
+if (!function_exists('versionedAsset'))
+    {
+        function versionedAsset($path)
+        {
+        if (Storage::disk('public')->exists($path))
+            {
+            return url('/').'/storage/' . $path;
+            }
+        if (app()->environment('local'))
+            {
+            $version = rand();
+            return asset($path) . '?v=' . $version;
+            }
+
+        return asset($path);
+        }
+    }
+if (!function_exists('authUser'))
+    {
+    function authUser()
+        {
+        return Auth::user();
+        }
+    }
