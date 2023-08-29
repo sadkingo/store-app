2285fbe9cfa44f922be6684a8313e92efb71145f
SaDKinG <hker0004@gmail.com>
2023-08-29 22:52:18 +0100

    add ignore for edit for email owner

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -4,33 +4,36 @@ namespace App\Http\Requests;
 
 use App\Rules\StateCityRule;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 use Illuminate\Validation\Rules\Password;
 
 class RegisterRequest extends FormRequest
-{
+    {
     /**
      * Determine if the user is authorized to make this request.
      */
-    public function authorize(): bool
-    {
+    public function authorize() : bool
+        {
         return true;
-    }
+        }
 
     /**
      * Get the validation rules that apply to the request.
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
      */
-    public function rules(): array
-    {
+    public static function rules($userId = null) : array
+        {
         return [
-            'full_name'=>'required|string|min:2|max:255',
-            'username'=>'required|string|unique:users,username|min:5|max:255',
-            'password'=>['required','confirmed', Password::defaults()],
-            'address'=>'required|string|min:2|max:255',
-            'phone'=>'required|numeric|min:10',
-            'email'=>'required|email|unique:users,email|min:5|max:50',
-            'city'=>['required', new StateCityRule]
+            'full_name' => 'required|string|min:2|max:255',
+            'username'  => ['required', 'string', 'min:5', 'max:255'
+                , Rule::unique('users', 'username')->ignore($userId),],
+            'password'  => ['required', 'confirmed', Password::defaults()],
+            'address'   => 'required|string|min:2|max:255',
+            'phone'     => 'required|numeric|min:10',
+            'email'     => ['required', 'email', 'min:5', 'max:50'
+                , Rule::unique('users', 'email')->ignore($userId),],
+            'city'      => ['required', new StateCityRule],
         ];
+        }
     }
-}
