a7444acf3d9efaa6240d707a242cb84cba1f9728
SaDKinG <hker0004@gmail.com>
2023-08-31 19:42:00 +0100

    maked the asset dynamic

--- resources/views/layout/home/product-card.blade.php
+++ resources/views/layout/home/product-card.blade.php
@@ -1,9 +1,9 @@
 <article class="featured__card m-1">
 
-    <img src="{{ $product->image_url }}" alt="" class="featured__img">
+    <img src="{{ versionedAsset($product->image_url) }}" alt="" class="featured__img">
     <div class="featured__data">
         <h3 class="featured__title">{{ $product->name }}</h3>
-        <span class="featured__price">{{"$".$product->price }}</span>
+        <span class="featured__price">{{ "$" . $product->price }}</span>
     </div>
     <div class="featured__button">
         <a href="product/{{ $product->id }}" class="featured__button button button--gray button--small">Discover</a>
