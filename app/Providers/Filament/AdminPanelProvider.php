283b3670e119d427e4f9466ec104f85b5cde9403
SaDKinG <hker0004@gmail.com>
2023-08-31 19:40:22 +0100

    override the personal info

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -72,7 +72,7 @@ class AdminPanelProvider extends PanelProvider
                     shouldRegisterNavigation: false, // Adds a main navigation item for the My Profile page (default = false)
                     hasAvatars: false, // Enables the avatar upload form component (default = false)
                     slug: 'my-profile' // Sets the slug for the profile page (default = 'my-profile')
-                )->myProfileComponents([MyCustomComponent::class])
+                )->myProfileComponents(['personal_info' => MyCustomComponent::class])
             );
 
         }
