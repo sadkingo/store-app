0258353a43b25aebb862201c3f9a836ba02e6d69
SaDKinG <hker0004@gmail.com>
2023-08-31 19:42:48 +0100

    Adjusted routes

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\CheckoutController;
 use App\Http\Controllers\homeController;
+use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
@@ -23,17 +24,17 @@ Route::get('/', [homeController::class, 'index'])->name('home');
 Route::resource('product', ProductController::class)->only('index', 'show');
 
 
-Route::middleware('guest')->group(function ()
-    {
+Route::middleware('guest')->group(function () {
     Route::get('login', [AuthController::class, 'index'])->name('login');
     Route::post('login', [AuthController::class, 'store']);
     Route::get('register', [RegisterController::class, 'index'])->name('register');
     Route::post('register', [RegisterController::class, 'store']);
-    });
+});
 
-Route::middleware('auth')->group(function ()
-    {
+Route::middleware('auth')->group(function () {
     Route::get('logout', [AuthController::class, 'destroy'])->name('logout');
-    Route::post('cart',[CartController::class , 'store']);
-    Route::get('checkout',[CheckoutController::class , 'create'])->name('checkout');
-    });
+    Route::post('cart', [CartController::class, 'store'])->name('cart.store');
+    // Route::delete('cart/{id}', [CartController::class, 'destroy'])->name('cart.destroy');
+    Route::get('checkout/{id}', [CheckoutController::class, 'show'])->name('checkout.show');
+    Route::post('order/{id}', [OrderController::class, 'store'])->name('order.sotre');
+});
