eb79229c8f45cc8232bb259e47f81633ca0f0ca8
SaDKinG <hker0004@gmail.com>
2023-08-31 19:38:02 +0100

    Added store to Order controller

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use App\Models\Order;
 use App\Http\Requests\StoreOrderRequest;
 use App\Http\Requests\UpdateOrderRequest;
+use App\Models\Cart;
+use App\Models\CartItem;
+use App\Models\OrderItem;
 
 class OrderController extends Controller
 {
@@ -27,9 +30,42 @@ class OrderController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreOrderRequest $request)
+    public function store(StoreOrderRequest $request, $id)
     {
-        //
+
+        $requestValidated = $request->validated();
+        // split the state city
+        $stateCityArray = explode('-', $requestValidated['city']);
+        $requestValidated['state_id'] = $stateCityArray[0];
+        $requestValidated['city_id'] = $stateCityArray[1];
+
+        // grab the cart by the checkout id
+        $cart = Cart::with(['items.product', 'user'])->findOrFail($id);
+        // check if the user is the owner of the cart
+        if (authUser()->id != $cart->user_id) {
+            abort(403);
+        }
+
+        // calc cart total
+        $cartTotal = 0;
+        foreach ($cart->items as $item) {
+            $cartTotal += $item->product->price * $item->quantity;
+        }
+
+        // creating order
+        $order =  Order::create([
+            'user_id' => $cart->user->id,
+            'total_amount' => $cartTotal,
+            'address' => $requestValidated['address'],
+            'city_id' => $requestValidated['city_id'],
+            'phone' => $requestValidated['phone'],
+        ]);
+        foreach ($cart->items as $item) {
+            $order->items()->create($item->toArray());
+        }
+
+        $cart->delete();
+        return to_route('filament.dashboard.pages.dashboard');
     }
 
     /**
