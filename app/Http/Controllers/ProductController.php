8dd244d5e527d0e5d46f90fd63b05e6ba57d3dad
SaDKinG <hker0004@gmail.com>
2023-08-31 19:38:33 +0100

    add random products to shwo product controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,62 +7,67 @@ use App\Http\Requests\StoreProductRequest;
 use App\Http\Requests\UpdateProductRequest;
 
 class ProductController extends Controller
-{
+    {
     /**
      * Display a listing of the resource.
      */
     public function index()
-    {
+        {
         $products = Product::paginate(12);
-        return view('products.index' ,compact('products'));
-    }
+        return view('products.index', compact('products'));
+        }
 
     /**
      * Show the form for creating a new resource.
      */
     public function create()
-    {
+        {
         //
-    }
+        }
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(StoreProductRequest $request)
-    {
+        {
         //
-    }
+        }
 
     /**
      * Display the specified resource.
      */
     public function show($id)
-    {
-       $product = Product::findOrFail($id);
-        return view('products.show' ,compact('product'));
-    }
+        {
+        $product = Product::with('images')->findOrFail($id);
+        $randomProducts = Product::with('images')
+            ->inRandomOrder()
+            ->where('id', '!=', $id)
+            ->take(5)
+            ->get();
+        return view('products.show', compact('product','randomProducts'));
+        }
 
     /**
      * Show the form for editing the specified resource.
      */
     public function edit(Product $product)
-    {
+        {
         //
-    }
+        }
 
     /**
      * Update the specified resource in storage.
      */
     public function update(UpdateProductRequest $request, Product $product)
-    {
+        {
         //
-    }
+        }
 
     /**
      * Remove the specified resource from storage.
      */
     public function destroy(Product $product)
-    {
+        {
         //
+        }
     }
-}
