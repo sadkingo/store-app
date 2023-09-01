003d4ce7da12af020e0311bfebbb75a50066347e
SaDKinG <hker0004@gmail.com>
2023-09-01 01:23:18 +0100

    Added PositionTrait trait

--- app/Models/ProductImage.php
+++ app/Models/ProductImage.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use App\Traits\PositionTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ProductImage extends Model
     {
-    use HasFactory;
+    use HasFactory,PositionTrait;
     protected $fillable =[
         'product_id',
         'image_url',
